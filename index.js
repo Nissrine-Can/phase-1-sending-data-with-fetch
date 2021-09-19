a0d1ebbc056552615c554d9338defd3bc089eb44
Nissrine Canina <niss.canina@gmail.com>
2021-09-19 15:20:47 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Add your code here
+
+function submitData(name, email) {
+
+    const formData = {
+        name: "Steve",
+        email: "steve@steve.com",
+      };
+      
+      const configurationObject = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+        body: JSON.stringify(formData),
+      };
+
+    return fetch('http://localhost:3000/users', configurationObject)
+    .then(response => response.json())
+    .then(formData => {
+        document.body.innerHTML = formData.id;
+    })
+    .catch(function (error) {
+        alert("Unauthorized Access");
+        return document.body.innerHTML = error.message;
+      });
+
+}
+
